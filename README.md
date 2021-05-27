f23f668b74e23d30cf061f8de94a8fd26e4bb0ff
Herbert Carvalho <betoo.carvalho.167@gmail.com>
2021-05-27 17:43:39 -0300

    config
    Configuração de database.php na pasta config para interligar a aplicação com o banco de dados. Index chamando databade.php

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # innout
- sistema de ponto eletronico com php e bootsprap
+## Sistema de ponto eletronico com php, bootsprap e mysql
+
+Este sistema é o módulo final do curso de "PHP 7 Completo - Curso de Desenvolvimento Web 2021 + Projetos" que está hospedado na Udemy(https://www.udemy.com/course/php-7-completo/).
+
+
+### O sistema está contido de diversas funcionalidades do Padrão MVC.
+- Model
+- View
+- Controller
+
+
+### Todo o template é feito em bootstrap.
+
+
+### E no banco de dados é usado mysql.
---
+++ src/config/database.php
@@ -0,0 +1,22 @@
+<?php
+
+// realpath() expande todos os links simbólicos e resolve referências para '/./', '/../' e extra caracteres '/' na entrada pelo path, e retorna o path absoluto canonicalizado.
+// parse_ini_file() carrega o arquivo INI especificado em filename e retorna as configurações contidas nele em um array associativo.
+// mysqli - Representa uma conexão entre PHP e um banco de dados MySQL.
+
+
+class Database {
+
+    public static function getConnection() {
+        $envPath = realpath(dirname(__FILE__) . '/../env.ini');
+        $env = parse_ini_file($envPath);
+        $conn = new mysqli($env['host'], $env['username'], 
+            $env['password'], $env['database']);
+
+        if($conn->connect_error) {
+            die("Erro: " . $conn->connect_error);
+        }
+
+        return $conn;
+    }
+}
