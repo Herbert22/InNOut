4f3ae2e0bd2ee3173a704752d87c7e3514102d4c
Herbert Carvalho <betoo.carvalho.167@gmail.com>
2021-06-12 15:36:42 -0300

    login/logout
    finalizado o template e incrementado o login/logout

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -1,5 +1,6 @@
 <?php
 loadModel('Login');
+session_start();
 $exception = null;
 
 // count — Conta o número de elementos de uma variável, ou propriedades de um objeto
@@ -7,6 +8,7 @@ if(count($_POST) > 0) {
     $login = new Login($_POST);
     try {
         $user = $login->checkLogin();
+        $_SESSION['user'] = $user;
         header("Location: day_records.php");
     } catch(AppException $e) {
         $exception = $e;
